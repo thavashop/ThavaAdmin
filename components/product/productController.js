b31374e4677a313f255517927312846d46d7640c
Thành <37612705+becamegod@users.noreply.github.com>
2021-12-01 12:09:49 +0700

    Improve UX

--- components/product/productController.js
+++ components/product/productController.js
@@ -4,7 +4,13 @@ const imageMineTypes = ['image/jpg', 'image/png', 'image/gif', 'image/jpeg']
 exports.list = async function (req, res) {
     try {
         const products = await Product.find({})
-        res.render('index', { products: products });
+        let success
+        let error
+        console.log(req.query);
+        if (req.query.del == 1) success = 'Product deleted'
+        if (req.query.del == 0) error = "There's a problem deleting product"
+        if (req.query.edt == 1) success = 'Product editted'
+        res.render('index', { products: products, success: success, error: error });
     } catch {
         console.log('err getting products');
         res.render('index')
@@ -22,7 +28,7 @@ exports.add = async function (req, res) {
         price: body.price,
         description: body.description,
     })
-    saveImage(product, body.image)    
+    saveImage(product, body.image)
     // console.log(product);
 
     try {
@@ -37,40 +43,53 @@ exports.renderEdit = async function (req, res) {
     try {
         const product = await Product.findById(req.params.id)
         renderEditPage(res, product)
-    } catch {
-        console.log('something wrong');
+    } catch (err) {
+        console.log(err);
+        res.redirect('products')
     }
 }
 
-exports.edit = async function(req, res) {
+exports.edit = async function (req, res) {
+    const id = req.params.id
+    let product
     try {
-        const id = req.params.id
         const body = req.body
-        const dummy = new Product()
-        saveImage(dummy, body.image)
-        const product = await Product.updateOne({_id: id}, {$set: {
-            name: body.name,
-            price: body.price,
-            description: body.description,
-            image: dummy.image,
-            imageType: dummy.imageType
-        }})
-        console.log(product);
-        res.redirect('/products') // feedback
-    } catch {
-        console.log('something wrong');
+        // const dummy = new Product()
+        // saveImage(dummy, body.image)
+        // const result = await Product.updateOne({_id: id}, {$set: {
+        //     name: body.name,
+        //     price: body.price,
+        //     description: body.description,
+        //     image: dummy.image,
+        //     imageType: dummy.imageType
+        // }})
+        // console.log(result);
+        product = await Product.findById(id)
+        with (product) {
+            name = body.name
+            price = body.price
+            description = body.description
+        }
+        saveImage(product, body.image)
+        await product.save()
+        // renderEditPage(res, product, 1)
+        res.redirect('/products?edt=1')
+    } catch (err) {
+        console.log(err);
+        renderEditPage(res, product, -1)
     }
 }
 
-exports.delete = async function(req, res) {
+exports.delete = async function (req, res) {
     try {
-        const result = await Product.deleteOne({_id: req.params.id})
+        const result = await Product.deleteOne({ _id: req.params.id })
         console.log(result);
-        res.redirect('/products')
-        // some feed back?        
-    } catch {
-        console.log('something wrong');
-        res.redirect('/products')
+        // res.render('/products', { success: 'Product deleted' })
+        res.redirect('/products?del=1')
+    } catch (err) {
+        console.log(err);
+        // res.render('/products', { error: "There's a problem deleting product" })
+        res.redirect('/products?del=0')
     }
 }
 
@@ -79,10 +98,11 @@ async function renderAddPage(res, product, flag = 0) {
         const params = {
             product: product
         }
-        if (flag == -1) params.errMessage = 'Error creating product'
+        if (flag == -1) params.error = 'Error creating product'
         else if (flag == 1) params.success = 'Product created'
         res.render('./products/add', params)
-    } catch {
+    } catch (err) {
+        console.log(err);
         res.redirect('products')
     }
 }
@@ -92,10 +112,11 @@ async function renderEditPage(res, product, flag = 0) {
         const params = {
             product: product
         }
-        if (flag == -1) params.errMessage = 'Error editting product'
+        if (flag == -1) params.error = 'Error editting product'
         else if (flag == 1) params.success = 'Product editted'
         res.render('products/edit', params)
-    } catch {
+    } catch (err) {
+        console.log(err);
         res.redirect('products')
     }
 }
